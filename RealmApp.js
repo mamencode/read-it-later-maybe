5771c42440dafb7fd68824a6426bee49f63afe09
Mamen code <mamencode@gmail.com>
2022-06-27 13:49:30 +0000

    cd

--- RealmApp.js
+++ RealmApp.js
@@ -1,7 +1,7 @@
 import Realm from "realm";
 
 // Shared instance of the Realm app.
-const app = new Realm.App({id: "your-realm-app-id-here"}); // Set Realm app ID here.
+const app = new Realm.App({id: "application-0-rsfvy"}); // Set Realm app ID here.
 // to find your Realm App ID see: https://docs.mongodb.com/realm/get-started/find-your-project-or-app-id/#find-a-realm-application-id 
 // You need to create a Free MongoDB Atlas Database, and a Realm App connected to that DB
 
